00058acbc36cc4ecdb4ee94912999836c03aec45
bo yang <595883397@qq.com>
2016-05-18 15:19:26 +0800

    绘制立方体

---
+++ app/src/main/java/com/cy/yangbo/album3d/util/Cube.java
@@ -0,0 +1,31 @@
+package com.cy.yangbo.album3d.util;
+
+import android.opengl.GLSurfaceView;
+
+import java.nio.FloatBuffer;
+
+/**
+ * Created by Administrator on 2016/5/17.
+ */
+public class Cube {
+
+    private GLSurfaceView mGLSurfaceView;
+    private FloatBuffer mCoordBuffer;
+
+    public Cube(GLSurfaceView glSurfaceView){
+        this.mGLSurfaceView = glSurfaceView;
+    }
+
+    public void initVertexData(){
+
+    }
+
+    public void initShader(GLSurfaceView glSurfaceView){
+
+    }
+
+    public void drawSelf(){
+
+    }
+}
+
--- app/src/main/java/com/cy/yangbo/album3d/view/AlbumView.java
+++ app/src/main/java/com/cy/yangbo/album3d/view/AlbumView.java
@@ -1,6 +1,7 @@
 package com.cy.yangbo.album3d.view;
 
 import android.content.Context;
+import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
 import android.util.AttributeSet;
 
@@ -25,17 +26,19 @@ public class AlbumView extends GLSurfaceView{
 
         @Override
         public void onSurfaceCreated(GL10 gl, EGLConfig config) {
-
+            GLES20.glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
+            GLES20.glEnable(GLES20.GL_DEPTH_TEST);
         }
 
         @Override
         public void onSurfaceChanged(GL10 gl, int width, int height) {
-
+            GLES20.glViewport(0, 0, width, height);
+            float ratio = (float)width / height;
         }
 
         @Override
         public void onDrawFrame(GL10 gl) {
-
+            GLES20.glClear(GLES20.GL_DEPTH_BUFFER_BIT | GLES20.GL_COLOR_BUFFER_BIT);
         }
     }
 }
